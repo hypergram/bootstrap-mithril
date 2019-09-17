85cc88a74d78063e2f7d82dae794d9b327e8c08b
Nick Goodliff <nick@hypergram.co>
2019-09-17 12:08:21 +0100

    Update navbar.js

--- packages/bootstrap-mithril-navbar/src/navbar.js
+++ packages/bootstrap-mithril-navbar/src/navbar.js
@@ -1,39 +1,39 @@
-import m from "mithril";
+import m from 'mithril';
 
 const navbar = {};
 navbar.view = vnode => {
 	const attrs = vnode.attrs;
 	return m(
-		"nav",
+		'nav',
 		{
 			className: [
-				"navbar",
-				attrs.toggler ? "navbar-expand-lg" : null,
-				`navbar-${attrs.color || "light"}`,
-				`bg-${attrs.colorBg || (attrs.color || "light")}`,
-			].join(" "),
+				'navbar',
+				attrs.toggler ? 'navbar-expand-lg' : null,
+				`navbar-${attrs.color || 'light'}`,
+				`bg-${attrs.colorBg || (attrs.color || 'light')}`,
+			].join(' '),
 		},
 		[
 			attrs.brand
 				? m(
-					"a.navbar-brand",
-					{
-						href: attrs.brand.href ? attrs.brand.href : "/",
-						oncreate: m.route.link,
-					},
-					attrs.brand.title
+						'a.navbar-brand',
+						{
+							href: attrs.brand.href ? attrs.brand.href : '/',
+							oncreate: m.route.link,
+						},
+						attrs.brand.title
 				  )
 				: null,
 			attrs.toggler
 				? m(
-					"button.navbar-toggler navbar-toggler-right",
-					{
-						type: "button",
-						"data-toggle": "collapse",
-						"aria-expanded": "false",
-						"aria-label": "Toggle navigation",
-					},
-					[m("span.navbar-toggler-icon")]
+						'button.navbar-toggler.navbar-toggler-right',
+						{
+							type: 'button',
+							'data-toggle': 'collapse',
+							'aria-expanded': 'false',
+							'aria-label': 'Toggle navigation',
+						},
+						[m('span.navbar-toggler-icon')]
 				  )
 				: null,
 			attrs.navitems,
